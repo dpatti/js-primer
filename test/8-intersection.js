80bea6ee44564254c5cd80b57f9ecfa07458a8e4
Doug Patti <douglas.patti@gmail.com>
2015-04-13 16:44:40 -0400

    add a few intersection tests

--- test/8-intersection.js
+++ test/8-intersection.js
@@ -9,6 +9,18 @@ it('returns an empty array for no second argument', function(){
   expect(intersection([1, 2, 3], [])).to.eql([]);
 });
 
+it('handles equal sets', function(){
+  expect(intersection([1, 2, 3], [3, 2, 1])).to.eql([1, 2, 3]);
+});
+
+it('works with a heavy first set', function(){
+  expect(intersection([1, 2, 3], [1])).to.eql([1]);
+});
+
+it('works with a heavy second set', function(){
+  expect(intersection([1], [1, 2, 3])).to.eql([1]);
+});
+
 it('does not coerce values', function(){
   expect(intersection([1, 2], ['1', 2])).to.eql([2]);
 });
